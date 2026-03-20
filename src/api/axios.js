888b8c5a2aac9c424d93de9247e337300a568e95
Jose Pablo Hernandez Raymundo <josepablohr@outlook.es>
2026-03-20 13:55:37 -0600

    correciones

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5142/api',
+    baseURL: 'https://localhost:7172/api',
     timeout: 10000, 
     headers: {
         'Content-Type': 'application/json',
